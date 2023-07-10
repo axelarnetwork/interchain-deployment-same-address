176dfaf713850c1c0c95a6c4d42dd54854c6d624
benjamin852 <weinbergben33@gmail.com>
2023-07-10 11:53:31 -0400

    cleaned up hardhat config code

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,32 +1,24 @@
 require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-ethers");
 
-const MUMBAI = `https://polygon-mumbai.g.alchemy.com/v2/${process.env.MUMBAI}`;
-const AVALANCHE = `https://avalanche-fuji.infura.io/v3/${process.env.AVALANCHE}`;
+const MUMBAI_RPC = `https://polygon-mumbai.g.alchemy.com/v2/${process.env.MUMBAI}`;
+const AVALANCHE_RPC = `https://avalanche-fuji.infura.io/v3/${process.env.AVALANCHE}`;
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks: {
     mumbai: {
-      url: MUMBAI,
+      url: MUMBAI_RPC,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
       network_id: 80001,
       timeoutBlocks: 200,
-      gas: 6000000, // Use around 4836635 in total.
-      gasLimit: 210000,
-      gasPrice: 4000000000, // Check standard gas price at https://gasstation-mumbai.matic.today/
       skipDryRun: true,
     },
     avalanche: {
-      url: AVALANCHE,
+      url: AVALANCHE_RPC,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
       network_id: 43113,
       timeoutBlocks: 200,
-      gas: 6000000, // Use around 4836635 in total.
-      gasPrice: 25000000000, // Check standard gas price at https://gasstation-mumbai.matic.today/
-      gasLimit: 210000,
       skipDryRun: true,
     },
   },

cd76273ad5b3dbd71e67093b0d65c0518b178d04
benjamin852 <weinbergben33@gmail.com>
2023-07-10 11:42:35 -0400

    cleaned up log in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,8 +5,6 @@ const {
 const LockAbi = require("../artifacts/contracts/Lock.sol/Lock.json");
 const chains = require("../chains.json");
 
-const hre = require("hardhat");
-
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
@@ -19,14 +17,8 @@ async function main() {
     const provider = getDefaultProvider(chain.rpc);
     const wallet = walletInit.connect(provider);
     const lockContract = await deployContract(wallet, LockAbi, [unlockTime]);
-    console.log("contract address:", lockContract.address);
+    console.log(`${chain.name} contract address: ${lockContract.address}`);
   });
-
-  // console.log(
-  //   `Lock with ${ethers.formatEther(
-  //     lockedAmount
-  //   )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  // );
 }
 
 function getEvmChains() {

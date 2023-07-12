6140f56f6b4816ec5e189a3fcac4e1b0b0cd446c
benjamin852 <weinbergben33@gmail.com>
2023-07-12 11:00:18 -0400

    typescript deploy-with-nonce

--- scripts/deploy-with-nonce.ts
+++ scripts/deploy-with-nonce.ts
@@ -1,32 +1,26 @@
-const { Wallet, getDefaultProvider } = require('ethers');
-const {
-    utils: { deployContract },
-} = require('@axelar-network/axelar-local-dev');
-const LockAbi = require('../artifacts/contracts/Lock.sol/Lock.json');
-const chains = require('../chains.json');
-
-const hre = require('hardhat');
+import { Wallet, getDefaultProvider, utils } from 'ethers';
+import { deployContract } from '@axelar-network/axelar-local-dev';
+import LockAbi from '../artifacts/contracts/Lock.sol/Lock.json';
+import chains from '../chains.json';
 
 async function main() {
     const currentTimestampInSeconds = Math.round(Date.now() / 1000);
     const unlockTime = currentTimestampInSeconds + 60;
 
-    const chains = getEvmChains();
+    const evmChains = getEvmChains();
 
     const walletInit = new Wallet(process.env.PRIVATE_KEY);
 
-    chains.map(async (chain) => {
+    for (const chain of evmChains) {
         const provider = getDefaultProvider(chain.rpc);
         const wallet = walletInit.connect(provider);
         const lockContract = await deployContract(wallet, LockAbi, [unlockTime]);
         console.log(`${chain.name} contract address: ${lockContract.address}`);
-    });
+    }
 }
 
 function getEvmChains() {
-    return chains.map((chain) => ({
-        ...chain,
-    }));
+    return chains.map((chain) => ({ ...chain }));
 }
 
 main().catch((error) => {

3651a945d06f89f4d5db855d41bc5a02c9973d0c
benjamin852 <weinbergben33@gmail.com>
2023-07-12 10:36:04 -0400

    cleaned up network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,21 +13,11 @@ const config: HardhatUserConfig = {
             url: chains[0].rpc,
             accounts: [`0x${process.env.PRIVATE_KEY}`],
             network_id: 80001,
-            timeoutBlocks: 200,
-            gas: 6000000, // Use around 4836635 in total.
-            gasLimit: 210000,
-            gasPrice: 4000000000, // Check standard gas price at https://gasstation-mumbai.matic.today/
-            skipDryRun: true,
         },
         avalanche: {
             url: chains[1].rpc,
             accounts: [`0x${process.env.PRIVATE_KEY}`],
             network_id: 43113,
-            timeoutBlocks: 200,
-            gas: 6000000, // Use around 4836635 in total.
-            gasPrice: 25000000000, // Check standard gas price at https://gasstation-mumbai.matic.today/
-            gasLimit: 210000,
-            skipDryRun: true,
         },
     },
 };

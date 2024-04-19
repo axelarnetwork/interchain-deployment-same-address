13ece6b1f921fb75910f89fcd161616efc5899fa
benjamin852 <weinbergben33@gmail.com>
2024-04-19 16:57:55 -0400

    chore: refactor polygon and avalanche for celo and fantom

--- scripts/deploy-create2.ts
+++ scripts/deploy-create2.ts
@@ -12,27 +12,33 @@ async function main() {
         throw new Error('Invalid private key. Make sure the PRIVATE_KEY environment variable is set.');
     }
 
+    const initData = encodeInitData('0xc5DcAC3e02f878FE995BF71b1Ef05153b71da8BE');
     const evmChains = getEvmChains();
-    
+
     for (const chain of evmChains) {
         const wallet = new Wallet(privateKey);
-        const initData = encodeInitData(wallet.address);
         const provider = getDefaultProvider(chain.rpc);
         const connectedWallet = wallet.connect(provider);
+
+        // Fetch current gas price and increase it
+        const gasPrice = await provider.getGasPrice();
+
 
         const deployerContract = new ethers.Contract(CONST_ADDRESS_DEPLOYER_ADDR, ConstAddressDeployer.abi, connectedWallet);
 
         //salt (make sure this salt has not been used already)
-        const salt = ethers.utils.hexZeroPad(BigNumber.from(550).toHexString(), 32);
+        const salt = ethers.utils.hexZeroPad(BigNumber.from(237).toHexString(), 32);
+
 
         const deployedAddr = await deployerContract.deployAndInit(Lock.bytecode, salt, initData);
         const receipt = await deployedAddr.wait();
-
         console.log(`${chain.name}, address: ${receipt.events[0].args.deployedAddress}`);
+
+
     }
 }
 
-function encodeInitData(wallet) {
+function encodeInitData(wallet: string) {
     const currentTimestampInSeconds = Math.round(Date.now() / 1000);
     const unlockTime = currentTimestampInSeconds + 60;
 
